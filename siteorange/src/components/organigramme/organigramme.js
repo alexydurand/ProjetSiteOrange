57c24420533468cb8917b10e5ec320ca4647e413
DURAND Alexy DISU <HGXP4007@orange.com>
2026-03-23 11:50:09 +0100

    MAJ organigramme

--- siteorange/src/components/organigramme/organigramme.js
+++ siteorange/src/components/organigramme/organigramme.js
@@ -5,17 +5,20 @@ function AccordionItem({ id, title, children }) {
   return (
     <div className="accordion-item">
       <h2 className="accordion-header">
-        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#${id}`}>
+        <button
+          className="accordion-button"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target={`#${id}`}
+        >
           {title}
         </button>
       </h2>
       <div id={id} className="accordion-collapse collapse show">
-        <div className="accordion-body">
-          {children}
-        </div>
+        <div className="accordion-body">{children}</div>
       </div>
     </div>
-  )
+  );
 }
 
 /* fonction Organigramme qui présente la hierarchie d'Orange*/
@@ -31,17 +34,55 @@ function Organigramme() {
                 <div className="accordion" id="accordionDISU">
                   <AccordionItem id="collapseDISU" title="DISU">
                     <div className="accordion" id="accordionDTI">
-                      <AccordionItem id="collapseDTI" title="Direction Technique et Intégration">
+                      <AccordionItem
+                        id="collapseDTI"
+                        title="Direction Technique et Intégration"
+                      >
                         <div className="accordion" id="accordionAPV">
                           <AccordionItem id="collapseAPV" title="APV">
-                            <ul><li>Kimberley CRUNEL (Manager)</li></ul>
+                            <ul>
+                              <li>
+                                <a
+                                  href="https://annuaire-sec.sso.infra.ftgroup/persons/57lppgs_1IADjHYH_9OurA%3D%3D"
+                                  target="_blank"
+                                  rel="noreferrer"
+                                >
+                                  Kimberley CRUNEL (Manager)
+                                </a>
+                              </li>
+                            </ul>
                             <div className="row mt-3">
                               <div className="col-md-6">
                                 <div className="accordion" id="accordionAMEC">
                                   <AccordionItem id="collapseAMEC" title="AMEC">
                                     <ul>
-                                      <li>Matthys VERBREGUE</li>
-                                      <li>Alexy DURAND</li>
+                                      <li>
+                                        <a
+                                          href="https://annuaire-sec.sso.infra.ftgroup/persons/H2hHoAuidzcDjHYH_9OurA%3D%3D"
+                                          target="_blank"
+                                          rel="noreferrer"
+                                        >
+                                          Alexy DURAND
+                                        </a>
+                                      </li>
+                                      <li>
+                                        <a
+                                          href="https://annuaire-sec.sso.infra.ftgroup/persons/kHnuF836I5sDjHYH_9OurA%3D%3D"
+                                          target="_blank"
+                                          rel="noreferrer"
+                                        >
+                                          Matthys VERBREGUE
+                                        </a>
+                                      </li>
+                                      <li>
+                                        <a
+                                          href="https://annuaire-sec.sso.infra.ftgroup/persons/iKExlMp2iP0DjHYH_9OurA%3D%3D"
+                                          target="_blank"
+                                          rel="noreferrer"
+                                        >
+                                          Corentin ROBIC
+                                        </a>
+                                      </li>
                                     </ul>
                                   </AccordionItem>
                                 </div>
@@ -49,7 +90,15 @@ function Organigramme() {
                               <div className="col-md-6">
                                 <div className="accordion" id="accordionBAVO">
                                   <AccordionItem id="collapseBAVO" title="BAVO">
-                                    <ul><li>Alexis VALLET</li></ul>
+                                    <li>
+                                      <a
+                                        href="https://annuaire-sec.sso.infra.ftgroup/persons/XwEqZNjQXUoDjHYH_9OurA%3D%3D"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                      >
+                                        Alexis VALLET
+                                      </a>
+                                    </li>
                                   </AccordionItem>
                                 </div>
                               </div>
@@ -65,10 +114,23 @@ function Organigramme() {
           </div>
           <div className="col-md-6">
             <div className="accordion" id="accordionDTOF">
-              <AccordionItem id="collapseDTOF" title="Direction Technique Orange France">
+              <AccordionItem
+                id="collapseDTOF"
+                title="Direction Technique Orange France"
+              >
                 <div className="accordion" id="accordionPFC">
                   <AccordionItem id="collapsePFC" title="PFC">
-                    <ul><li>Chloé CHIARLINI</li></ul>
+                    <ul>
+                      <li>
+                        <a
+                          href="https://annuaire-sec.sso.infra.ftgroup/persons/W00P7xV20J0DjHYH_9OurA%3D%3D"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Chloé CHIARLINI
+                        </a>
+                      </li>
+                    </ul>
                   </AccordionItem>
                 </div>
               </AccordionItem>
@@ -77,7 +139,7 @@ function Organigramme() {
         </div>
       </AccordionItem>
     </div>
-  )
+  );
 }
 
 export default Organigramme;
